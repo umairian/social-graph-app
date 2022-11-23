296fe8f5088969fee2bf4373c89e86ddc3403e73
Umair Syed <itsumairsyed@gmail.com>
2022-11-23 20:55:14 +0500

    User model code removed from user controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,8 @@
-const { Users } = require("../models");
 
 module.exports = {
   get: async (req, res) => {
     try {
-      const users = await Users.findAll();
-      return res.status(200).send({ users });
+      return res.status(200).send("Users");
     } catch (err) {
       console.log(err);
       return res.status(err.status || 500).message(err.message || "Something went wrong...");
