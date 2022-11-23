cc76307e61e99c0960eb87744a81a937495a8485
Umair Syed <itsumairsyed@gmail.com>
2022-11-23 22:25:46 +0500

    Graphql server added

--- server.js
+++ server.js
@@ -1,7 +1,42 @@
 const config = require("./config");
 const app = require("./app.js");
 const port = config.get("port");
+const { ApolloServer, gql } = require('apollo-server');
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen(port, () => {
+  console.log(`🚀 Server running at port: ${port}`);
 });
