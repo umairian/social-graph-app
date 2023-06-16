27e7eba954b9505d9a89aaaabc2bc56a2997d6ad
Umair Syed <itsumairsyed@gmail.com>
2023-06-16 11:48:48 +0500

    Message model created

---
+++ models/message.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const schema = new Schema({
+  _id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    unique: true
+  },
+  sender: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  recipient: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now
+  }
+});
+
+module.exports = model("Message", schema);
